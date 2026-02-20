012dac4c221e2462a9607fc4a7932254c4c43e62
John <johnrojas298@gmail.com>
2026-02-20 16:06:11 -0500

    correcion del puerto

--- app.js
+++ app.js
@@ -480,4 +480,9 @@ app.post('/restore-pedidos', async (req, res) => {
 });
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'tienda.html')); });
+
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
